c77f57b478a53e1672f2cf95e5914f9150b6b4af
pedro henrique <revan271205@gmail.com>
2025-04-09 10:26:40 -0300

    commit final

--- aula4/TelaCarro.py
+++ aula4/TelaCarro.py
@@ -4,16 +4,26 @@ import sys
 from Carro import Carro
 
 class TelaCarro(TelaVeiculo):
-    def __init__(self, titulo="tela do carro"):
+    def __init__(self, titulo="tela do carro", categorias = []):
         super().__init__(titulo)
         self.setGeometry(450, 150, 300, 200)
-
+        self.categorias = categorias
     def definirLayout(self):
         super().definirLayout()
         self.lblportas = QLabel("Qtd Portas: ")
         self.txtportas = QLineEdit(self)
         self.layout.addWidget(self.lblportas)
         self.layout.addWidget(self.txtportas)
+        self.lblCategoria = QLabel("Categoria")
+        
+        self.layout.addWidget(self.lblCategoria)
+
+        self.cmbCategoria = QComboBox(self)
+        self.cmbCategoria.addItem("Selecione...", None)
+
+        for cat in self.categorias:
+            self.cmbCategoria.addItem(cat.nome , cat)
+        self.layout.addWidget(self.cmbCategoria)
 
     def salvar(self):
         modelo = self.txtmodelo.text()
