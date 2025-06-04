263cabe60241e26f4755001d6c18961db6ec8cec
pedro <revan271205@gmail.com>
2025-06-04 11:24:59 -0300

    lista duplamente encadeada 2/6

---
+++ aula10/main.py
@@ -0,0 +1,17 @@
+from listaDuplamenteEncadeada import listaDuplamenteEncadeada
+
+lista = listaDuplamenteEncadeada()
+lista.imprimir
+lista.add("jonatas")
+lista.add("adalto")
+lista.add("guilherme")
+lista.add("sarah")
+lista.add("pedro")
+
+lista.imprimirReversa()
+lista.remover("João")
+lista.remover("guilherme")
+lista.remover("adalto")
+lista.remover("sarah")
+
+lista.imprimirReversa()
